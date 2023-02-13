20c8db5476bbd42faa6c171e2d22e7ab0aa37642
emmanuelkiplangatbett <emmanuel.bett@student.moringa>
2023-02-13 21:50:26 +0300

    completed assignment

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,34 @@
 class Triangle
   # write code here
+  attr_reader :a, :b, :c
+
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+    validate
+  end
+
+  def kind
+    if a == b && b == c
+      :equilateral
+    elsif a == b || b == c || a == c
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  private
+
+  def validate
+    raise TriangleError, "Sides must be greater than 0" if [a, b, c].min <= 0
+    raise TriangleError, "This is not a valid triangle" if a + b <= c || a + c <= b || b + c <= a
+  end
+
+  class TriangleError < StandardError
+  end
 end
+
+
+
